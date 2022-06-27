2930ed3a266ac6f36b92a04dcd95cf5831c52c71
rockwoodc <rockwoodce@gmail.com>
2022-06-27 11:55:20 -0400

    get user info

--- assets/JS/homepage.js
+++ assets/JS/homepage.js
@@ -1,5 +1,15 @@
-var getUserReops = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+//user allows us to input any user on github
+var getUserReops = function(user) {
+    //format the github api url with the user info
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
 };
 
 getUserReops();
